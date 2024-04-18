4ef969dfec18c0efbbde3d5ca10dc16759a648f0
Marvin Wiechers <Marvin.Wiechers@adesso.de>
2024-04-18 17:33:57 +0200

    changed grid to flex

--- client/src/components/pages/fitness-home/fitness-home.ts
+++ client/src/components/pages/fitness-home/fitness-home.ts
@@ -17,23 +17,61 @@ export class HomeComponent extends LitElement {
       
 <body>
     <main class="main-content">
-        <div class="content-area">
-            <h1>Fitness mit Verstand</h1>
-            <p>
-                Willkommen bei All in One Fitness! Hier findest du alles, was du für ein gesundes Leben brauchst. 
-                Ob Muskelaufbau, Abnehmen oder einfach nur fit bleiben - wir helfen dir dabei, deine Ziele zu erreichen. 
-                Mit unseren Trainingsplänen und Ernährungstipps bist du bestens gerüstet, um fit und gesund zu bleiben.
-            </p>
-            <p>
-                Unsere Trainingspläne sind speziell auf deine Bedürfnisse zugeschnitten und helfen dir dabei, deine Ziele zu erreichen. 
-                Egal ob du Anfänger oder Profi bist, bei uns findest du das passende Programm für dich. 
-                Mit unseren Ernährungstipps kannst du deine Leistung steigern und dich rundum wohlfühlen. 
-                Wir zeigen dir, wie du dich gesund ernähren kannst und dabei trotzdem genießen darfst.
-            </p>
-        </div>
+        <div class="two-column-row" id="first-row">
+            <div class="two-column-row-content">
+                <h1>Fitness mit Verstand</h1>
+                <p>
+                   Willkommen bei All in One Fitness! Hier findest du alles, was du für ein gesundes Leben brauchst. 
+                    Ob Muskelaufbau, Abnehmen oder einfach nur fit bleiben - wir helfen dir dabei, deine Ziele zu erreichen. 
+                    Mit unseren Trainingsplänen und Ernährungstipps bist du bestens gerüstet, um fit und gesund zu bleiben.
+             </p>
+             <p>
+                 Unsere Trainingspläne sind speziell auf deine Bedürfnisse zugeschnitten und helfen dir dabei, deine Ziele zu erreichen. 
+                 Egal ob du Anfänger oder Profi bist, bei uns findest du das passende Programm für dich. 
+                 Mit unseren Trainingseinheiten kannst du deine Leistung steigern und dich rundum wohlfühlen. Dabei kannst du deinen Traumkörper erreichen
+                 und wirst von unseren Trainern unterstützt.
+                </p>
+            <div class="link-button" >
+                <a href="/trainings-sessions">Jetzt starten</a>
+            </div>
+            </div>
         <div class="image-area">
             <img src="./liegestütze.png">
         </div>
+  </div>
+   <div  style="background-color: rgba(0, 0, 0, 0.1)">
+            <div class="mid-text">
+                <h2>Unser Angebot</h2>
+                <p>
+                    Um immer auf dem neuesten Stand zu bleiben, bieten wir dir regelmäßig neue Trainingspläne und Ernährungstipps. 
+                    Deine bisherigen Erfolge kannst du in unserem Ernährungstracker und Kalendar festhalten. Plane deine Trainingseinheiten und Mahlzeiten im Voraus und behalte so den Überblick über deine Fortschritte.
+                </p>
+            </div>
+        <div class="two-column-row">
+
+            <div class="two-column-row-content">
+                <h1>Unsere Philosophie</h1>
+                <p>
+                    Bei All in One Fitness steht der Mensch im Mittelpunkt. 
+                    Wir möchten dir dabei helfen, ein gesundes und glückliches Leben zu führen. 
+                    Deshalb bieten wir dir nicht nur ein umfangreiches Trainingsprogramm, sondern auch wertvolle Tipps für eine gesunde Ernährung. 
+                    Wir möchten, dass du dich rundum wohlfühlst und deine Ziele erreichst. 
+                    Deshalb unterstützen wir dich auf dem Weg zu einem gesunden und fitten Leben.
+                </p>
+            </div>
+
+            <div class="two-column-row-content">
+                <h1>Unsere Angebote</h1>
+                <p>
+                    Bei All in One Fitness findest du alles, was du für ein gesundes Leben brauchst. 
+                    Egal ob du Muskeln aufbauen, abnehmen oder einfach nur fit bleiben möchtest - wir haben das passende Angebot für dich. 
+                    Unsere Trainingspläne sind speziell auf deine Bedürfnisse zugeschnitten und helfen dir dabei, deine Ziele zu erreichen. 
+                    Mit unseren Ernährungstipps kannst du deine Leistung steigern und dich rundum wohlfühlen. 
+                    Wir zeigen dir, wie du dich gesund ernähren kannst und dabei trotzdem genießen darfst.
+                </p>
+            </div>
+        </div>
+    </div>
     </main>
 </body>
 
---
+++ client/src/components/widgets/my-header/my-header.ts
@@ -0,0 +1,28 @@
+//Autor: Marvin Wiechers
+import {LitElement, css, html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+import componentStyle from './my-header.css?inline';
+
+@customElement('my-header')
+class MyHeader extends LitElement {
+  static styles = [componentStyle] ;
+  render() {
+    return html`
+      <header>
+      <div class= "collapse navbar-collapse  auto-responsive isScrollTop"  id="myNavbar">
+      
+        <div class="row" style="overflow: visible">
+        
+              <ul class= "nav navbar-nav">
+                <li data-page><a href="/fitness-home">Home</a></li>
+                <li data-page><a href="/trainings-sessions">Trainingseinheiten</a></li>
+                <li data-page><a href="/nutrition-tracker">Ernährungstracker</a></li>
+                <li data-page><a href="/kalendar">Kalendar</a></li>
+                <li data-page><a href="/login">Login</a></li>
+              </ul>
+            </div>
+          </div>
+      </header>
+    `;
+  }
+}
