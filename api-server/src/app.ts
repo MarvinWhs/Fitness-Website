cd19ef7eea6aa826062873eb5291bc0a61c5dccb
mw746554 <mw746554@fh-muenster.de>
2024-05-21 13:59:03 +0200

    DAtenbank ist hinzugefügt und eingebunden

--- api-server/src/app.ts
+++ api-server/src/app.ts
@@ -10,8 +10,12 @@ import config from '../config.json' assert { type: 'json' };
 import exerciseRoute from './Routes/exercise.route.js';
 
 function configureApp(app: Express) {
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+  app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({
+  limit: '5mb',
+  extended: true,
+  parameterLimit: 5000
+}));
   app.use(cookieParser());
   app.use(cors());
   app.use(exerciseRoute);
