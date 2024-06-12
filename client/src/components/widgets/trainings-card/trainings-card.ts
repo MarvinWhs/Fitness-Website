70f53b7fed5c9f7bd2bbada79ea79ba74f92e15d
Niklas Lobo <nl717609@fh-muenster.de>
2024-06-12 16:45:48 +0200

    .

--- client/src/components/widgets/trainings-card/trainings-card.ts
+++ client/src/components/widgets/trainings-card/trainings-card.ts
@@ -6,6 +6,7 @@ import { Notificator } from '../notificator/notificator.js';
 import { HttpClient, httpClientContext } from './../../../http-client.js';
 import { consume } from '@lit/context';
 import config from '../../../../config.json';
+import '../trainings-timer/countdown-widget.js';
 
 interface Exercise {
   id: string;
